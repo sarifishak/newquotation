47e599e2f612479238d6836b09b32f63db4d3b5c
Sarif Ishak <sarif.ishak@gmail.com>
2016-11-09 19:25:40 +0800

    dbmanager.php is using dbmanager.inc as setting file for db access

--- dbmanager.php
+++ dbmanager.php
@@ -1,19 +1,5 @@
 <?php
-class DBManager{
 
-  function getConnection(){
-    
-	// sarif add here at test_git_3
-	// sarif add here now (9/11/2016 6:20PM) at test_git_2
-	// sarif add here also at test_git_3 at 9/11/2016 6:21PM
-   
-    // using PDO approach
-    $conn = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    
-    return $conn;
-    
-  }
-}
+    require_once 'dbmanager.inc';
+
 ?>
