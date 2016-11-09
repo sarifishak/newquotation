a9f172f1e74da30af5feabaac9cab8ba954cd65b
Sarif Ishak <sarif.ishak@gmail.com>
2016-11-09 18:22:26 +0800

    modify dbmanager again at 6:21pm

--- dbmanager.php
+++ dbmanager.php
@@ -4,6 +4,8 @@ class DBManager{
   function getConnection(){
     
 	// sarif add here at test_git_3
+	
+	// sarif add here also at test_git_3 at 9/11/2016 6:21PM
    
     // using PDO approach
     $conn = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $username, $password);
