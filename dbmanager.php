b1958ee78f2ee58fd9e49a8e1f2b5f52f4e13eab
Sarif Ishak <sarif.ishak@gmail.com>
2016-11-09 18:16:52 +0800

    modify dbmanager a bit

--- dbmanager.php
+++ dbmanager.php
@@ -2,7 +2,8 @@
 class DBManager{
 
   function getConnection(){
-
+    
+	// sarif add here at test_git_3
    
     // using PDO approach
     $conn = new PDO('mysql:host='.$host.';dbname='.$db.';charset=utf8mb4', $username, $password);
