25e47245018b5ffcb26cfe46c1c395b4aa97007e
Sarif Ishak <sarif.ishak@gmail.com>
2018-01-19 10:54:29 +0800

    Small update

--- dbmanager.php
+++ dbmanager.php
@@ -3,38 +3,14 @@ class DBManager{
 
   function getConnection(){
 
-    /* 
-    // this for using cloud connection
-    $services = getenv("VCAP_SERVICES");
-    $services_json = json_decode($services,true);
-    $mysql_config = $services_json["cleardb"][0]["credentials"];
 
-    $db = $mysql_config["name"];
-    $host = $mysql_config["hostname"];
-    $port = $mysql_config["port"];
-    $username = $mysql_config["username"];
-    $password = $mysql_config["password"];
-    
-    
-    $conn = mysql_connect($host . ':' . $port, $username, $password);
-    if(! $conn ){
-      die('Could not connect:db=' .$db . ',host=' . $host . ',port=' . $port . ',username=' . $username . ',password=' . $password . ',error=' . mysqli_error($conn));
-    }
-    */
-    
-    // this for using local connection
-
-    $db = "ad_a42b01dedd03768"; 
+    $db = "XXXXXXX"; 
     $host = "localhost";
     $port = "3306";
     // this setting for VM
-    $username = "shima";
-    $password = "p@ssw0rd";
+    $username = "XXXXXX";
+    $password = "XXXXXX";
     
-    //this setting for my local
-    //$username = "sarif";
-    //$password = "norlaily*282";
- 
     /*
     $conn = mysqli_connect($host,$username,$password,$db);
 
